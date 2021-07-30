64f75dc7cd321a68d91cd0a1aad995c5d0c4d09b
Fadi_Nayef <adraf38@hotmail.com>
2021-07-30 15:53:22 +0300

    Initial commit

---
+++ src/main/java/com/songrApp/songrApp/SongrAppApplication.java
@@ -0,0 +1,13 @@
+package com.songrApp.songrApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songrApp/songrApp/SongrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songrApp.songrApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
